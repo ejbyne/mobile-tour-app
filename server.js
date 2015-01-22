39c093c7be4ca913361e913027e3a7034aa83abb
Ming Chan <ming.chan@windowslive.com>
2015-01-22 12:25:25 +0000

    added heatmap and Tweets to map

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/mapinfo', function(request, response) {
   var latitude = request.query.latitude;
   var longitude = request.query.longitude;
 
-  https.get("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" + latitude + "," + longitude + "&radius=1000&types=bar&key=" + key, function(apiResponse) {
+  https.get("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" + latitude + "," + longitude + "&radius=400&types=bar&key=" + key, function(apiResponse) {
     var buf = '';
     apiResponse.setEncoding('utf8');
     apiResponse.on('data', function(data) {
@@ -92,5 +92,64 @@ app.get('/placeinfo/:place_id', function(request, response) {
 //   console.log(placesData);
 // });
 
+
+var twitterMarker = function(callback) {
+  var array = new Array();
+  var stream = fs.createReadStream('./tweets.txt', {flags: 'r', encoding: 'utf-8'});
+  var buf = '';
+
+  stream.on('data', function(d) {
+    buf += d.toString(); // when data is read, stash it in a string buffer
+    pump(); // then process the buffer
+  }).on('end', function(){
+    callback(null, array)
+    console.log(array.length)
+  })
+
+  function pump() {
+    var pos;
+    while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+      process(buf.slice(0,pos)); // hand off the line
+      buf = buf.slice(pos+1); // and slice the processed data off the buffer
+    }
+  }
+
+  function process(line) { // here's where we do something with a line
+  if (line.length > 0) { // ignore empty lines
+    // parse the JSON
+    isJson(line)
+  }
+}
+
+function isJson(str) {
+  try {
+    var obj = JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  if (obj.geo != null && latIsFine(obj.geo.coordinates[0]) && longIsFine(obj.geo.coordinates[1])){
+    array.push(obj.geo.coordinates);
+    return true;
+  } else{
+    return true;
+  }
+}
+
+function latIsFine (latitude){
+  return latitude >= 51.514492 && latitude <= 51.520853
+}
+
+function longIsFine(longitude){
+  return longitude >= -0.080306 && longitude <= -0.06515
+}
+}
+
+app.get('/markers', function(request, response){
+  twitterMarker(function(err, markers){
+    if(err)
+      response.send(err)
+      response.json(markers)
+    });
+  });
 
 module.exports = server;
