e59ed51524ec74127b53db3fe693de301f109017
ed <ejbyne@gmail.com>
2015-01-22 15:40:19 +0000

    map working for commercial street area

---
+++ public/js/app.js
@@ -0,0 +1,115 @@
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function error(err) {
+  alert('Error(' + err.code + '): ' + err.message);
+};
+
+function initialize(position) {
+
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(latitude, longitude),
+    scaleControl: true
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var infoWindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  var glass = 'img/glass.svg';
+  var man = 'img/man.svg';
+
+  var currentPositionMarker = new google.maps.Marker({
+    position: new google.maps.LatLng(latitude, longitude),
+    map: map,
+    icon: new google.maps.MarkerImage(man, null, null, null, new google.maps.Size(36, 36))
+  });
+
+  $.get( "/tweetinfo", function( data ) {
+      var array = []
+      // console.log(data)
+      data.forEach(function(tweet){
+      // createMarker(tweet);
+        array.push(new google.maps.LatLng(tweet[0],
+                                    tweet[1]))
+      });
+      var pointArray = new google.maps.MVCArray(array);
+      heatmap = new google.maps.visualization.HeatmapLayer({
+        data: pointArray
+      });
+      heatmap.set('radius', 16);
+      heatmap.set('opacity', 1);
+      // heatmap.set('dissipating', true);
+      heatmap.setMap(map);
+    });
+
+  // function createMarker(place) {
+  //   var mposition = new google.maps.LatLng(place[0],
+  //                                   place[1]);
+  //   var marker = new google.maps.Marker({
+  //     map: map,
+  //     position: mposition
+  //   });
+  // };
+
+  $.get('/mapinfo?latitude=' + latitude + '&longitude=' + longitude, function(data) {
+    data.results.forEach(function(place) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(place.geometry.location.lat, place.geometry.location.lng),
+        map: map,
+        icon: new google.maps.MarkerImage(glass, null, null, null, new google.maps.Size(24,24))
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        service.getDetails(place, function(result, status) {
+          if (status != google.maps.places.PlacesServiceStatus.OK) {
+            alert(status);
+            return;
+          }
+         
+          var name = result.name + '<br>';
+
+          var address = result.vicinity + ' ' +
+                        result.address_components[result.address_components.length-1].long_name + '<br>';
+
+          var website = function() {
+            if (result.website) {
+              var text = '<a href="' + result.website + '">' + result.website + '</a>';
+              return text;
+            }
+            else {
+              return '';
+            }
+          };
+
+          var openingHours = function() {
+            if ( result.opening_hours) {
+              var text = '<br>Opening Hours:<br>'
+              result.opening_hours.weekday_text.forEach(function(day) {
+                text += day + '<br>'
+              });
+              return text;
+            }
+            else {
+              return '';
+            }
+          };
+
+          var details = name +
+                        address +
+                        website() +
+                        openingHours();
+          infoWindow.setContent(details);
+          infoWindow.open(map, marker);
+        });
+      });
+    });
+  });
+}
+
+navigator.geolocation.getCurrentPosition(initialize, error, options);
--- server.js
+++ server.js
@@ -4,21 +4,103 @@ var server = require('http').createServer(app);
 var Twit = require('twit');
 var fs = require('fs');
 var https = require('https');
+var fs = require ('fs')
 // var bodyParser = require('body-parser')
 
 var port = process.env.PORT || 8000;
 
 var placesData;
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }))
 
-server.listen(port, function() {
-  console.log('Server listening on port ' + port);
+var twitterMarker = function(callback) {
+  var array = new Array();
+  var stream = fs.createReadStream('./tweets.txt', {flags: 'r', encoding: 'utf-8'});
+  var buf = '';
+
+  stream.on('data', function(d) {
+    buf += d.toString(); // when data is read, stash it in a string buffer
+    pump(); // then process the buffer
+  }).on('end', function(){
+    callback(null, array) 
+    console.log(array.length)
+  })
+
+  function pump() {
+    var pos;
+    while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+      process(buf.slice(0,pos)); // hand off the line
+      buf = buf.slice(pos+1); // and slice the processed data off the buffer
+    }
+  }
+
+  function process(line) { // here's where we do something with a line
+    if (line.length > 0) { // ignore empty lines
+    // parse the JSON
+    isJson(line)
+    }
+  }
+
+  function isJson(str) {
+    try {
+      var obj = JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    if (obj.geo != null && latIsFine(obj.geo.coordinates[0]) && longIsFine(obj.geo.coordinates[1])){
+      array.push(obj.geo.coordinates);
+      return true;
+    } else{
+      return true;
+    }
+  }
+
+  function latIsFine (latitude){
+    return latitude >= 51.514492 && latitude <= 51.520853
+  }
+
+  function longIsFine(longitude){
+    return longitude >= -0.080306 && longitude <= -0.06515
+  }
+}
+
+var key = process.env.GOOGLE_PLACES_KEY;
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.get('/tweetinfo', function(request, response){
+  twitterMarker(function(err, markers){
+    if(err)
+      response.send(err)
+        response.json(markers)
+  });
+});
+
+app.get('/mapinfo', function(request, response) {
+  var latitude = request.query.latitude;
+  var longitude = request.query.longitude;
+
+  https.get("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" + latitude + "," + longitude + "&radius=400&types=bar&key=" + key, function(apiResponse) {
+    var buf = '';
+    apiResponse.setEncoding('utf8');
+    apiResponse.on('data', function(data) {
+      buf += data;
+    }).on('end', function() {
+      placesData = JSON.parse(buf);
+      response.json(placesData);
+    });
+  });
+});
+
+app.get('/placeinfo/:place_id', function(request, response) {
+  console.log(request.params.place_id);
 });
 
 // var auth = new Twit({
@@ -46,51 +128,9 @@ server.listen(port, function() {
 //   });
 // });
 
-var key = process.env.GOOGLE_PLACES_KEY;
 
-app.get('/', function(request, response) {
-  response.render('index');
+server.listen(port, function() {
+  console.log('Server listening on port ' + port);
 });
-
-app.get('/mapinfo', function(request, response) {
-  var latitude = request.query.latitude;
-  var longitude = request.query.longitude;
-
-  https.get("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" + latitude + "," + longitude + "&radius=1000&types=bar&key=" + key, function(apiResponse) {
-    var buf = '';
-    apiResponse.setEncoding('utf8');
-    apiResponse.on('data', function(data) {
-      buf += data;
-    }).on('end', function() {
-      placesData = JSON.parse(buf);
-      response.json(placesData);
-    });
-  });
-});
-
-app.get('/placeinfo/:place_id', function(request, response) {
-  console.log(request.params.place_id);
-});
-    // fs.appendFile("./places.txt", data, function(err) {
-    //       if(err) {
-    //
-    //       } else {
-    //
-    //       }
-    // });
-  //   console.log(data);
-  // });
-//
-// }).on('error', function(e) {
-//   console.log("Got error: " + e.message);
-//
-// });
-
-// fs.readFile('./places.txt', 'utf8', function (err, data) {
-//   if (err) throw err;
-//   placesData = data;
-//   console.log(placesData);
-// });
-
 
 module.exports = server;
